a36d3c3c7d2cc99442d4b71cd63a93c14028a948
javpet <javorkai.peter@gmail.com>
2018-02-10 22:16:15 +0100

    Fixed carrierwave issue

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     Rails.appplication.secrets.aws_key,                        # required
-    aws_secret_access_key: Rails.appplication.secrets.aws_secret,                        # required
+    aws_access_key_id:     Rails.application.secrets.aws_key,                        # required
+    aws_secret_access_key: Rails.application.secrets.aws_secret,                        # required
     region:                'us-east-2'
   }
   config.fog_directory  = 'noto-development'            # required
