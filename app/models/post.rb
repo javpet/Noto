c114e2369f92632fa06c5f69ccabf320e273a276
javpet <javorkai.peter@gmail.com>
2018-02-17 21:30:01 +0100

    Friendly URL parameters for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,8 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { minimum: 3 }
   validates :body, presence: true, length: { in: 10..400 }
 
+  def to_param
+    id.to_s + "-" + title.parameterize
+  end
+
 end
